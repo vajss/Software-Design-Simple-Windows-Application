21c9d191d0d1d0c44d6218afbb20bf95ea70b772
Vajss <vasilije1998@gmail.com>
2024-02-04 12:30:24 +0100

    Add project files.

---
+++ Software Design - Simple Windows Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_Design___Simple_Windows_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
