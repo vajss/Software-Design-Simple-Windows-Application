1e7ed2031c93d5fe1de04a5f2bb1618344b11f2b
Vajss <vasilije1998@gmail.com>
2024-02-04 12:34:32 +0100

    First commit

--- Software Design - Simple Windows Application/Program.cs
+++ Software Design - Simple Windows Application/Program.cs
@@ -10,6 +10,8 @@ namespace Software_Design___Simple_Windows_Application
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
